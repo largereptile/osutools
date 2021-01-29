da69ed3763ca530b99d3331eba6620d447525072
harry <harry@yark.org.uk>
2021-01-29 14:52:05 +0000

    added support for getting scores from user object, including getting scores specifically since the last call of get_new_recents

--- pyosu/User.py
+++ pyosu/User.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from .Utils import Mode, Playtime
 
 
@@ -28,7 +28,30 @@ class User:
         self.country_rank = int(user_info['pp_country_rank'])
         seconds = int(user_info['total_seconds_played'])
         self.playtime = Playtime(seconds)
+        self.recents = {
+            Mode.STANDARD: [],
+            Mode.TAIKO: [],
+            Mode.CTB: [],
+            Mode.MANIA: []
+        }
         self.avatar_url = f"http://s.ppy.sh/a/{self.id}"
+
+    def fetch_best(self, mode: Mode = Mode.STANDARD, limit: int = 10):
+        return self.client.fetch_user_best(user_id=self.id, mode=mode, limit=limit)
+
+    def fetch_recent(self, mode: Mode = Mode.STANDARD, limit: int = 10):
+        self.recents[mode] = self.client.fetch_user_recent(user_id=self.id, mode=mode, limit=limit)
+        return self.recents[mode]
+
+    def fetch_new_recent(self, mode: Mode = Mode.STANDARD):
+        now = datetime.now()
+        self.recents[mode] = [score for score in self.recents[mode] if (now - timedelta(hours=24)) <= score.timestamp <= now]
+        new_scores = self.client.fetch_user_recent(user_id=self.id, mode=mode, limit=100)
+        if new_scores:
+            dates = [score.timestamp for score in self.recents[mode]]
+            new = [score for score in new_scores if score.timestamp not in dates]
+            self.recents[mode] += new
+            return new
 
     def __repr__(self):
         return self.username
