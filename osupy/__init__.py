596d0e4c9ddf26c1db2159351fa68a2a908cfd91
harry <harry@yark.org.uk>
2021-05-19 01:16:16 +0100

    added new score/map files

--- osupy/__init__.py
+++ osupy/__init__.py
@@ -1,6 +1,7 @@
 from .osuclient import OsuClient
 from .user import User
-from .map import Map
-from .score import Score, RecentScore, MultiScore
+from .map import Map, LocalMap
+from .score import Score, RecentScore, MultiScore, LocalScore
 from .match import Match, Game
+from .db import OsuDB, ScoresDB, Collections
 from . import utils
