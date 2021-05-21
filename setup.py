2d62f892075130238db1c547413aa205bd765123
harry <harry@yark.org.uk>
2021-05-21 01:15:33 +0100

    README.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='osu!tools',
-    version='0.2.0',
+    version='0.2.1',
     packages=['osutools'],
     url='https://github.com/largereptile/osutools',
     license='MIT',
@@ -15,5 +15,11 @@ setup(
     description='A python wrapper for working with the osu! API, databases and file formats',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['requests>=2.25.1']
+    install_requires=['requests>=2.25.1'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
 )
