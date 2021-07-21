988f304bf57d58ce6fc515d13645821d7dfe92a7
harry <harry@yark.org.uk>
2021-07-21 01:04:40 +0100

    add test files

---
+++ tests/test_db.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+from dotenv import load_dotenv
+from pathlib import Path
+import json
+
+import osutools
+
+load_dotenv()
+
+token = os.environ["OSU_V1_TOKEN"]
+
+
+@pytest.fixture(scope="module", autouse=True)
+def load_client():
+    client = osutools.OsuClientV1(token)
+    client.set_osu_folder(Path.cwd() / "test_files" / "osu")
+    yield client
+
+
+def test_read_osu_db(load_client):
+    assert load_client.osu_db.player_name == "flubb 4"
+    assert load_client.osu_db.account_unlocked
+    assert load_client.osu_db.number_of_beatmaps == len(load_client.osu_db.maps)
+
+
+def test_search_local_maps(load_client):
+    sofia = load_client.osu_db.search_maps("sofia")
+    assert sofia and sofia[0].mapset_id == 1346543
+
+
+def test_export_osu_db(load_client):
+    json_path = Path("test_files") / "osu_db.json"
+    load_client.osu_db.export(json_path)
+    with open(json_path, "r") as f:
+        osu_db = json.load(f)
+    assert osu_db["player_name"] == "flubb 4"
+    os.remove(json_path)
